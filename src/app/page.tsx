3b41dfe74b2cfdb5c62c25145386d2b067125f27
prdel24 <brozikmrozik7@gmail.com>
2024-09-05 18:24:14 +0200

    setup nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <h1>Stepan Broz</h1>
+    </div>
+  );
+}
