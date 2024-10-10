8c7ff9a37f9651660f97b93ef2742b86cea7616a
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-10 09:56:17 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default async function Home() {
       <main className="max-w-2xl p-4 mx-auto">
         <h1 className="mb-5 text-2xl text-[#ffffff]">stepan</h1>
         <p className="text-[16px] leading-[28px] text-[#adadad]">
-          I&apos;m 17 y/o high school student. I love building stuff and solving problems. I enjoy building beautiful interactive websites with next js. If im not coding im probably out in the sun,
-          working out or reading.
+          I&apos;m 17 y/o high school student. I love building stuff and solving problems. I enjoy building beautiful, interactive and optimized web apps with next js. If im not coding im probably out
+          in the sun, working out or reading.
         </p>
         <TechnologiesUsing />
         <Projects />
