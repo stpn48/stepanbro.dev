979bb75b8fc8e6063ef985715f62602846f79ed4
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-21 19:53:33 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import React from "react";
 
 export default async function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)] pt-8 min-h-screen bg-[#0a0a0a]">
+    <div className="font-[family-name:var(--font-geist-sans)] py-8 min-h-screen">
       <main className="max-w-2xl p-4 mx-auto">
         <h1 className="mb-5 text-2xl text-[#ffffff]">stepan</h1>
         <p className="text-[16px] leading-[28px] text-[#adadad]">
           I&apos;m 17 y/o high school student. I love building stuff and solving problems. I enjoy building beautiful, interactive and optimized web apps with next js. If im not coding im probably
-          outside.
+          getting sorted.
         </p>
         <TechnologiesUsing />
         <Projects />
