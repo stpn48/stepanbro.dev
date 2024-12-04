81e7b6a2d721c3c157fd7d4c552fdfcdfbb7d747
stpn48 <133258815+stpn48@users.noreply.github.com>
2024-12-04 22:25:23 +0100

    feat: renmae stack to tools, add shadcn to tools

--- src/app/components/Tools.tsx
+++ src/app/components/Tools.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const technologies = [
   {
@@ -26,17 +27,22 @@ const technologies = [
     imgSrc: "/tsLogo.svg",
     https: "https://www.typescriptlang.org",
   },
+  {
+    name: "Shadcn",
+    imgSrc: "/shadcn-logo.svg",
+    https: "https://ui.shadcn.com/",
+  },
 ];
 
-export function TechnologiesUsing() {
+export function Tools() {
   return (
     <div className="flex flex-col font-geist">
-      <h1 className="mt-16 text-xl text-[#ffffff]">stack</h1>
+      <h1 className="mt-16 text-xl text-[#ffffff]">tools</h1>
       <div className="flex items-center gap-5 mt-6 w-fit">
         {technologies.map((technology) => (
-          <a key={technology.name} href={technology.https} title={technology.name} target="_blank" rel="noopener noreferrer">
+          <Link key={technology.name} href={technology.https} title={technology.name} target="_blank" rel="noopener noreferrer">
             <Image src={technology.imgSrc} alt={technology.name} width={28} height={28} />
-          </a>
+          </Link>
         ))}
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Links } from "@/app/components/Links";
 import { Projects } from "@/app/components/Projects";
-import { TechnologiesUsing } from "@/app/components/TechnologiesUsing";
+import { Tools } from "@/app/components/Tools";
 import React from "react";
 
 export default async function Home() {
@@ -8,11 +8,8 @@ export default async function Home() {
     <div className="font-[family-name:var(--font-geist-sans)] py-8 min-h-screen">
       <main className="max-w-2xl p-4 mx-auto">
         <h1 className="mb-5 text-2xl text-[#ffffff]">stepan</h1>
-        <p className="text-[16px] leading-[28px] text-[#adadad]">
-          I&apos;m 17 y/o high school student. I love building stuff and solving problems. I enjoy building beautiful, interactive and optimized web apps with next js. If im not coding im probably
-          getting sorted.
-        </p>
-        <TechnologiesUsing />
+        <p className="text-[16px] leading-[28px] text-[#adadad]">I build fast and beautiful stuff with next.js</p>
+        <Tools />
         <Projects />
         <Links />
       </main>
